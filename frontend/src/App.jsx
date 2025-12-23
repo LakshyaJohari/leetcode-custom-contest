326a79cdfdd8df922563496914f058468663b789
LakshyaJohari <lakshyajohari2006@gmail.com>
2025-12-24 00:59:36 +0530

    Add penalty tracking and rating with updated vercel hotst 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -87,7 +87,7 @@ const App = () => {
 
     try {
       // REPLACE WITH YOUR BACKEND URL (Use localhost for testing)
-      const res = await axios.post('http://lee-con-tom.onrender.com/create-contest', {
+      const res = await axios.post('https://lee-con-tom.onrender.com/create-contest', {
         session_cookie: cookie, selected_tags: selectedTags, mode: mode
       });
       
@@ -110,7 +110,7 @@ const App = () => {
   const checkSubmissions = async () => {
     if (!isActive) return;
     try {
-      const res = await axios.post('http://lee-con-tom.onrender.com/check-status', {
+      const res = await axios.post('https://lee-con-tom.onrender.com/check-status', {
         username: username,
         problem_slugs: contest.map(p => p.titleSlug),
         contest_start_time: startTime
